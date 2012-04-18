0c5e13f64f92f44295c7903d25a1e8a5c938ccce
Ken Jones <ken.jones@marakana.com>
2012-04-17 17:02:38 -0700

    Refactored MainActivity to use a fragment

---
+++ workspace/Yamba/src/com/marakana/android/yamba/ComposeFragment.java
@@ -0,0 +1,97 @@
+package com.marakana.android.yamba;
+
+import winterwell.jtwitter.Twitter;
+import winterwell.jtwitter.TwitterException;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class ComposeFragment extends Fragment implements OnClickListener {
+	private static final String TAG = "ComposeFragment";
+	
+	private EditText mEditMsg;
+	private Toast mToast;
+
+	private Twitter mTwitter;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setRetainInstance(true);
+		
+        // Initialize the Twitter object
+        mTwitter = new Twitter("student", "password");
+        mTwitter.setAPIRootUrl("http://yamba.marakana.com/api");
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View top = inflater.inflate(R.layout.compose_fragment, container, false);
+		
+        mEditMsg = (EditText) top.findViewById(R.id.edit_msg);
+        Button buttonUpdate = (Button) top.findViewById(R.id.button_update);
+        buttonUpdate.setOnClickListener(this);
+        
+        // Initialize Toast object
+        mToast = Toast.makeText(getActivity().getApplicationContext(), null, Toast.LENGTH_LONG);
+        
+		return top;
+	}
+
+	@Override
+	public void onClick(View v) {
+		Log.v(TAG, "Button clicked");
+		int id = v.getId();
+		switch (id) {
+		case R.id.button_update:
+			// Process an Update Status button click
+			String msg = mEditMsg.getText().toString();
+			Log.v(TAG, "User entered: " + msg);
+			mEditMsg.setText("");
+			
+			if (!TextUtils.isEmpty(msg)) {
+				// Post the status update
+				new PostToTwitter().execute(msg);
+			}
+			
+			break;
+		default:
+			// We should never get here!
+		}
+	}
+	
+	private class PostToTwitter extends AsyncTask<String, Void, Integer> {
+
+		@Override
+		protected Integer doInBackground(String... args) {
+			int result = R.string.post_status_success;
+			
+			try {
+				mTwitter.setStatus(args[0]);
+			} catch (TwitterException e) {
+				Log.e(TAG, "Failed to post message", e);
+				result = R.string.post_status_fail;
+			}
+			
+			return result;
+		}
+
+		@Override
+		protected void onPostExecute(Integer result) {
+			mToast.setText(result);
+			mToast.show();
+		}
+		
+	}
+}
--- workspace/Yamba/src/com/marakana/android/yamba/MainActivity.java
+++ workspace/Yamba/src/com/marakana/android/yamba/MainActivity.java
@@ -1,86 +1,15 @@
 package com.marakana.android.yamba;
 
-import winterwell.jtwitter.Twitter;
-import winterwell.jtwitter.TwitterException;
-import android.app.Activity;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.support.v4.app.FragmentActivity;
 
-public class MainActivity extends Activity implements OnClickListener {
-	private static final String TAG = "MainActivity";
-	private EditText editMsg;
-	private Toast toast;
-	
-	private Twitter twitter;
+public class MainActivity extends FragmentActivity {
 	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
-        editMsg = (EditText) findViewById(R.id.edit_msg);
-        Button buttonUpdate = (Button) findViewById(R.id.button_update);
-        buttonUpdate.setOnClickListener(this);
-        
-        // Initialize the Twitter object
-        twitter = new Twitter("student", "password");
-        twitter.setAPIRootUrl("http://yamba.marakana.com/api");
-        
-        // Initialize Toast object
-        toast = Toast.makeText(this, null, Toast.LENGTH_LONG);
     }
 
-	@Override
-	public void onClick(View v) {
-		Log.v(TAG, "Button clicked");
-		int id = v.getId();
-		switch (id) {
-		case R.id.button_update:
-			// Process an Update Status button click
-			String msg = editMsg.getText().toString();
-			Log.v(TAG, "User entered: " + msg);
-			editMsg.setText("");
-			
-			if (!TextUtils.isEmpty(msg)) {
-				// Post the status update
-				new PostToTwitter().execute(msg);
-			}
-			
-			break;
-		default:
-			// We should never get here!
-		}
-	}
-	
-	private class PostToTwitter extends AsyncTask<String, Void, Integer> {
-
-		@Override
-		protected Integer doInBackground(String... args) {
-			int result = R.string.post_status_success;
-			
-			try {
-				twitter.setStatus(args[0]);
-			} catch (TwitterException e) {
-				Log.e(TAG, "Failed to post message", e);
-				result = R.string.post_status_fail;
-			}
-			
-			return result;
-		}
-
-		@Override
-		protected void onPostExecute(Integer result) {
-			toast.setText(result);
-			toast.show();
-		}
-		
-	}
 }

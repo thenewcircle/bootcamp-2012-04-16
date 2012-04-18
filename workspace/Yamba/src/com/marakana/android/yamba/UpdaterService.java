dd206a884bdd1503d472e173327c33da06f6b3cb
Ken Jones <ken.jones@marakana.com>
2012-04-18 15:48:41 -0700

    Updated UpdaterService to fetch timeline data

--- workspace/Yamba/src/com/marakana/android/yamba/UpdaterService.java
+++ workspace/Yamba/src/com/marakana/android/yamba/UpdaterService.java
@@ -1,5 +1,10 @@
 package com.marakana.android.yamba;
 
+import java.util.Date;
+import java.util.List;
+
+import winterwell.jtwitter.Twitter;
+import winterwell.jtwitter.TwitterException;
 import android.app.IntentService;
 import android.content.Intent;
 import android.util.Log;
@@ -14,6 +19,19 @@ public class UpdaterService extends IntentService {
 	@Override
 	protected void onHandleIntent(Intent arg0) {
 		Log.v(TAG, "onHandleIntent() invoked");
+		
+		try {
+			List<Twitter.Status> timeline = YambaApplication.getInstance().getTwitter().getHomeTimeline();
+			for (Twitter.Status status: timeline) {
+				String name = status.user.name;
+				String msg = status.text;
+				long id = status.id;
+				Date createdAt = status.createdAt;
+				Log.v(TAG, id + ": " + name + " posted at " + createdAt + ": " + msg);
+			}
+		} catch (TwitterException e) {
+			Log.w(TAG, "Failed to retrieve timeline data");
+		}
 	}
 
 }
